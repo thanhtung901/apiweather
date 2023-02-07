1914cb076230cdd26ebcca0988328d2d5fbf3f93
ThanhTung09 <102369873+tungkmt@users.noreply.github.com>
2023-02-07 15:05:04 +0700

    Update senseHAT.py

--- senseHAT.py
+++ senseHAT.py
@@ -1,18 +1,10 @@
 from sense_emu import SenseHat
-from CurrentWeather import CurrentWeather
-class DisplayWeather:
-    current_weather = ''
-    def __init__(self, current_weather):
-        self.current_weather = current_weather
-    def display(self):
-        sense_hat_emulator = SenseHat()
-        message = ("%s %sC %s wind speed %s km/h"
-        %(self.current_weather.getCity(),
-        self.current_weather.getTemperature(),
-        self.current_weather.getWeatherConditions(),
-        self.current_weather.getWindSpeed()))
-        sense_hat_emulator.show_message(message)
+from currentweather import getWeather
+
+def display(self):
+    sense_hat_emulator = SenseHat()
+    condition, temp, min_temp, max_temp, pressure, humidity, wind = getWeather('hanoi')
+    message = (f'HANOI: Temperature: {max_temp}, humidity: {humidity}, wind: {wind}')
+    sense_hat_emulator.show_message(message)
 if __name__ == "__main__":
-    current_weather = CurrentWeather('Toronto')
-    display_weather = DisplayWeather(current_weather)
-    display_weather.display()
+    display()
