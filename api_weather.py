6a770a83d1b64d4ca24c2e823356651f940bbdc7
ThanhTung09 <102369873+tungkmt@users.noreply.github.com>
2023-02-13 14:03:42 +0700

    Create api_weather.py

---
+++ api_weather.py
@@ -0,0 +1,30 @@
+
+import requests
+
+class getWeather():
+
+    condition = ''
+    temperature = ''
+    wind = ''
+    city = ''
+    def __init__(self, city):
+        apiUrl = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=c55cad7bdd0e7d82b0b958fdfd7ceeae"
+        json_data = requests.get(apiUrl).json()
+        self.condition = json_data['weather'][0]['main']
+        self.temperature = int(json_data['main']['temp'] - 273.15)
+        self.wind = json_data['wind']['speed']
+        self.city = city
+    def get_condition(self):
+        return self.condition
+    def get_city(self):
+        return self.city
+    def get_wind(self):
+        return self.wind
+    def get_temperature(self):
+        return self.temperature
+if __name__ == '__main__':
+    api = getWeather('hanoi')
+    print('wind: ',api.get_wind())
+    print('temperature: ',api.get_temperature())
+    print('city: ',api.get_city())
+    print('condition: ',api.get_condition())
